0062fee65b6fb8131639a67c800f68b8d7f7757e
AdrianaDiAvi <adriana.diaz.avila@intel.com>
2022-10-11 11:26:46 -0400

    Change to outoput to verify

--- pre-silicon.groovy
+++ pre-silicon.groovy
@@ -4,7 +4,7 @@ def AnalysisTools(){
   
         sh '''
             alias dotriage='docker run -i --rm -w `pwd` -v `pwd`:`pwd` -e no_proxy=".intel.com, 10.0.0.0/8" triage-builder'
-            dotriage ./build-database/generate-wiki-validation-report.py --collection "executions" --test > test1.json
+            dotriage ./build-database/generate-wiki-validation-report.py --collection "executions" --test > testfinal.json
         '''
     
   }
