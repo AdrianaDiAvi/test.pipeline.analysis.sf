62d11490c51a1ba334e734f53d1646329f613195
AdrianaDiAvi <adriana.diaz.avila@intel.com>
2022-10-11 14:26:32 -0400

    Add teesting 1

--- pre-silicon.groovy
+++ pre-silicon.groovy
@@ -40,7 +40,6 @@ pipeline {
             sh '''
             python3 -m venv .venv/
             source .venv/bin/activate
-            pip3 install -r requirements.txt
             curl -sSf -H "X-JFrog-Art-Api:${ARTIFACTORY_CREDS}" -O ${ARTIFACTORY_REPO}/docker-triage.tar
             docker load -i docker-triage.tar
             docker images
@@ -80,6 +79,9 @@ pipeline {
                         sh '''
                         echo "the third case"
                         source .venv/bin/activate
+                        pwd
+                        ls
+                        pip3 install -r requirements.txt
                         ./build-database/update-cumulus-validation-report.py  --release "22.18" --buildsdb "mongodb://presibuilds_ro:zCzRyEa9gJdAbU3@p1or1mon031.amr.corp.intel.com:7765,p2or1mon031.amr.corp.intel.com:7765,p3or1mon031.amr.corp.intel.com:7765/presibuilds?ssl=true&replicaSet=mongo7765" --cumulusdb "http://10.88.81.185:5000" --collection executions_v2218
                         echo "ready"
                         '''
