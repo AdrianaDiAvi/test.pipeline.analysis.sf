467342987832305c76526aecd8f758a8a9094ae1
AdrianaDiAvi <adriana.diaz.avila@intel.com>
2022-10-11 16:39:28 -0400

    Add arguments v

--- pre-silicon.groovy
+++ pre-silicon.groovy
@@ -85,7 +85,7 @@ pipeline {
                     case "Validation Report Cumulus":
                         sh '''
                         echo "the third case"
-                        ./build-database/update-cumulus-validation-report.py  --release "${VERSION}" --buildsdb "mongodb://presibuilds_ro:zCzRyEa9gJdAbU3@p1or1mon031.amr.corp.intel.com:7765,p2or1mon031.amr.corp.intel.com:7765,p3or1mon031.amr.corp.intel.com:7765/presibuilds?ssl=true&replicaSet=mongo7765" --cumulusdb "http://10.88.81.185:5000" --collection "executions_v2218"
+                        ./build-database/update-cumulus-validation-report.py  --release "v${VERSION}" --buildsdb "mongodb://presibuilds_ro:zCzRyEa9gJdAbU3@p1or1mon031.amr.corp.intel.com:7765,p2or1mon031.amr.corp.intel.com:7765,p3or1mon031.amr.corp.intel.com:7765/presibuilds?ssl=true&replicaSet=mongo7765" --cumulusdb "http://10.88.81.185:5000" --collection "executions_v2218"
                         '''
                         input('Do you want to proceed')
                         break
