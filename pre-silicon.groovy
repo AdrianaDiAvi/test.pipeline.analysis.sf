abd55754827453c20ea6f10a43c12d33b4d39d83
AdrianaDiAvi <adriana.diaz.avila@intel.com>
2022-10-19 12:43:29 -0400

    Testing 3

--- pre-silicon.groovy
+++ pre-silicon.groovy
@@ -105,7 +105,7 @@ pipeline {
                         
                         sh '''
                         alias dotriage='docker run -i --rm -w `pwd` -v `pwd`:`pwd` -e no_proxy=".intel.com, 10.0.0.0/8" triage-builder'
-                        dotriage ./build-database/generate-wiki-validation-report.py --collection "executions" > ${WORKSPACE}/${ONESOURCE_DIR_WIKI}/validation/pre_release_validation/release-pre-si-validation-v${VERSION}.md
+                        dotriage ./build-database/generate-wiki-validation-report.py --collection "executions" > ${ONESOURCE_DIR_WIKI}/validation/pre_release_validation/release-pre-si-validation-v${VERSION}.md
                         pwd
                         cd ${WORKSPACE}/${ONESOURCE_DIR_WIKI}/validation/pre_release_validation
                         ls
