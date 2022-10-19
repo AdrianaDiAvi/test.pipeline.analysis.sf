fc81fa849357090a30f218bc94fd62074201c38f
AdrianaDiAvi <adriana.diaz.avila@intel.com>
2022-10-19 13:35:00 -0400

    Testing 6

--- pre-silicon.groovy
+++ pre-silicon.groovy
@@ -1,10 +1,10 @@
 #!/usr/bin/env groovy
 def AnalysisTools(){
   checkout([$class: 'GitSCM', doGenerateSubmoduleConfigurations: false,
-    extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: "${Workspace}/ONESOURCE_DIR"]],
+    extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: "${Workspace}"]],
     submoduleCfg: [], branches: [[name: 'development']], userRemoteConfigs: [[credentialsId: 'one-source-token-personal',
     url: 'https://github.com/AdrianaDiAvi/test.pipeline.analysis.sf.git']]])
- dir("${WORKSPACE}/${ONESOURCE_DIR}"){
+ dir("${WORKSPACE}"){
   sh '''
     python3 send-data-wiki.py
     '''
