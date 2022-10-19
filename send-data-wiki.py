5c4f885ee94fef6a661856905b7b252f0b3fa065
AdrianaDiAvi <adriana.diaz.avila@intel.com>
2022-10-19 13:47:38 -0400

    Function 2

--- send-data-wiki.py
+++ send-data-wiki.py
@@ -1,5 +1,15 @@
 import os
 import const
 
-
 print('Mundo...')
+ONESOURCE_DIR_WIKI = os.getenv("ONESOURCE_DIR_WIKI")
+val_md_path = const.VALIDATION_REPORT_MD + os.getenv('VERSION') + '.md'
+print('Verifying if the file exists...')
+if os.path.exists(os.path.join(ONESOURCE_DIR_WIKI,val_md_path)):
+    print('Adding new entry to "release-validation-presil" menu')
+    with open(os.path.join(ONESOURCE_DIR_WIKI, const.VALIDATION_REPORT_MAIN_MENU), 'r+') as f:
+        content = f.read()
+        f.seek(0, 0)
+        new_release = f'### v{os.getenv("VERSION")}\n- [**Validation Details**](release-pre-si-validation-v{os.getenv("VERSION")}#v{os.getenv("VERSION")})\n'
+        f.write(new_release.rstrip('\r\n') + '\n' + content)
+else: print('The file doesnt exist')
