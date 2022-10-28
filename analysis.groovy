6437d74c8b54b7162eeb0fb256e0597a89d1b0f8
AdrianaDiAvi <adriana.diaz.avila@intel.com>
2022-10-28 16:41:51 -0400

    Add parameter

--- analysis.groovy
+++ analysis.groovy
@@ -1,13 +1,11 @@
-def validation_kpi(additionalArgs){
+def validation_kpi(flag_validation_kpi){
   checkout([$class: 'GitSCM', doGenerateSubmoduleConfigurations: false,
     extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: "${Workspace}"]],
     submoduleCfg: [], branches: [[name: 'development']], userRemoteConfigs: [[credentialsId: 'one-source-token-personal',
     url: 'https://github.com/AdrianaDiAvi/test.pipeline.analysis.sf.git']]])
  dir("${WORKSPACE}"){
-  sh '''
-    python3 send-data-wiki.py '${additionalArgs}'
-    '''
-    
+  sh "python3 send-data-wiki.py '${flag_validation_kpi}' "
+  echo "${flag_validation_kpi}"
   }
 }
 
